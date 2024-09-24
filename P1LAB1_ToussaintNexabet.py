e5855a97026e35fff0f77482344685e7d73ac216
Nexabet1121 <nexabettoussaint@gmail.com>
2024-09-24 14:21:53 -0400

    First Upload

---
+++ P1LAB1_ToussaintNexabet.py
@@ -0,0 +1,14 @@
+# nexabet
+#9/24/2024
+# P1LAB1
+
+#Get first name from user
+first_name = input("enter your first name: ")
+
+# Get last name from user
+last_name = input("enter your last name: ")
+
+# Display info server
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
+print()
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
